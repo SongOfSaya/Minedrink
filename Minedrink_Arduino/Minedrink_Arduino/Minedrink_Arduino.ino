9564fcd4cbef0659fe1ed61d0208847a30436526
刘腾飞 <liutengfei833@hotmail.com>
2017-08-03 14:54:37 +0800

    解决方案MCU转移到Arduino

---
+++ Minedrink_Arduino/Minedrink_Arduino/Minedrink_Arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Minedrink_Arduino.ino
+ Created:	2017/8/3 14:41:24
+ Author:	liute
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
