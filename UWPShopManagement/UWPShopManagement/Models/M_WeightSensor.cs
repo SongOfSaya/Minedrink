7cfc49246232072e03ebbb2d773da22b9fb35c70
刘腾飞 <liutengfei833@hotmail.com>
2017-08-27 06:13:28 +0800

    服务器方案确定,开始进行连接

--- UWPShopManagement/UWPShopManagement/Models/M_WeightSensor.cs
+++ UWPShopManagement/UWPShopManagement/Models/M_WeightSensor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections.Generic;
 namespace UWPShopManagement.Models
 {
     public class M_WeightSensor : M_SensorBase
